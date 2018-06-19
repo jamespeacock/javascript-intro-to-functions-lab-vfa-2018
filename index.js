2c60573536cfef68095fe4aff4eaf3b3145ee528
James Peacock <james.hoke.peacock@gmail.com>
2018-06-19 15:41:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ var whisper = function(word) {
   return word.toLowerCase();
 }
 
-var 
+function sayHiToGrandma(string)  {
+  if (string === "I love you, Grandma") {
+    return "I love you too!"
+  }
+  if (string.toUpperCase() === string) {
+    return "";
+  }
+  if (string.toLowerCase() === string) {
+    return "";
+  }
+}
